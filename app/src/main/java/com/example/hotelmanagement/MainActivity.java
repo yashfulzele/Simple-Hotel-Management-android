581e30a73bcae3e7200db24758d772379c30a440
yashfulzele <28-user_13@users.noreply.ccgit.in>
2021-04-15 11:43:04 +0530

    second commit final

--- app/src/main/java/com/example/hotelmanagement/MainActivity.java
+++ app/src/main/java/com/example/hotelmanagement/MainActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -20,8 +21,9 @@ public class MainActivity extends AppCompatActivity {
     private String fullname, username, email;
     private TextInputEditText serviceEt;
     private Button serviceBookingBt, roomBookingBt;
-    private static final String READDEATAILS_URL = "http://192.168.0.105/android_hms/readDetails.php";
+    private static final String READDETAILS_URL = "http://192.168.0.105/android_hms/readDetails.php";
     private static final String SERVICEBOOKING_URL = "http://192.168.0.105/android_hms/serviceBooking.php";
+    private static final String MYTAG = "mytag";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,6 +31,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         username = getIntent().getStringExtra("username");
+        Log.d(MYTAG, "hi, there");
 
         fullnameTv = findViewById(R.id.fullname);
         usernameTv = findViewById(R.id.username);
@@ -39,25 +42,28 @@ public class MainActivity extends AppCompatActivity {
 
         usernameTv.setText(username);
 
-        Handler handler = new Handler(Looper.getMainLooper());
-        handler.post(() -> {
-            String[] field = new String[1];
-            field[0] = "username";
-            String[] data = new String[1];
-            data[0] = username;
-            PutData putData = new PutData(READDEATAILS_URL, "POST", field, data);
-            if (putData.startPut()) {
-                if (putData.onComplete()) {
-                    String result = putData.getResult();
-                    String[] arrOfStr = result.split(" ");
-                    fullname = arrOfStr[0];
-                    email = arrOfStr[1];
+        if (!username.equals("")) {
+            Handler handler = new Handler(Looper.getMainLooper());
+            handler.post(() -> {
+                String[] field = new String[1];
+                field[0] = "username";
+                String[] data = new String[1];
+                data[0] = username;
+                PutData putData = new PutData(READDETAILS_URL, "POST", field, data);
+                if (putData.startPut()) {
+                    if (putData.onComplete()) {
+                        String result = putData.getResult();
+                        Log.d(MYTAG, result);
+                        String[] arrOfStr = result.split("_");
+                        fullname = arrOfStr[0];
+                        email = arrOfStr[1];
+                        Log.d(MYTAG, fullname);
+                        fullnameTv.setText(fullname);
+                        emailTv.setText(email);
+                    }
                 }
-            }
-        });
-
-        fullnameTv.setText(fullname);
-        emailTv.setText(email);
+            });
+        }
 
         serviceBookingBt.setOnClickListener(v -> {
             String service = String.valueOf(serviceEt.getText());
@@ -75,6 +81,7 @@ public class MainActivity extends AppCompatActivity {
                         if (putData.onComplete()) {
                             String result = putData.getResult();
                             if (result.equals("Service Success")) {
+                                serviceEt.getText().clear();
                                 Toast.makeText(this, result, Toast.LENGTH_SHORT).show();
                             } else {
                                 Toast.makeText(this, result, Toast.LENGTH_SHORT).show();
